66b485c29fcfc181db1ec8764cd6f2b42d132c7f
manuj2n <manu.j2n@gmail.com>
2014-12-02 16:35:01 +0100

    modif pour y0 et z

--- MicroStripPatch.py
+++ MicroStripPatch.py
@@ -15,40 +15,41 @@ def Lamda():
     return c/(Fr * sqrt(Er))
 
 print ("Longueur d'onde corrigée (m): ", Lamda())
+
+print ("Longueur d'onde (m): ", c/Fr)
+
 W = (c/(2 * Fr)) * sqrt(2 / (Er + 1))
 
+print ("Rapport W/h : ", W/h)
 
 Ereff = ((Er + 1)/2.0) + (((Er - 1)/2.0) * pow (1 + ((12.0 * h)/W), -(0.5)))
 print ("Effective Dielectric Ereff", Ereff)
 
-Leff = c/(2 * Fr * sqrt(Ereff))
-print ("L efficace Leff", Leff)
-
 DeltaL = 0.412 * h * ((Ereff + 0.3) / (Ereff - 0.258)) * (((W / h) + 0.264) / ((W / h) + 0.8))
 print ("Delta L : ", DeltaL)
 
-L = Leff - (2* DeltaL)
+Leff = c/(2 * Fr * sqrt(Ereff))
+print ("L efficace Leff", Leff)
+
+L = Leff - (2 * DeltaL)
 print ("dimension L (m) : ", L)
 print ("dimension W (m): ", W)
 
-Lg = 6 * h + L
+Lg = 6.0 * h + L
 print ("Lg : ", Lg)
 
-Wg = 6 * h + W
+Wg = 6.0 * h + W
 print ("Wg : ", Wg)
 
-Xf2 = L * 0.15
+Xf2 = (L/pi * acos(sqrt(3)/3))
 print ("position du via en m sur axe L (Xf) a partir du bord : ", Xf2)
 
-Z = 300 * (cos((pi * Xf2)/L) * cos((pi * Xf2)/L))
+Z = 300.0 * (cos((pi * Xf2)/L) * cos((pi * Xf2)/L))
 print ("Impedance du patch: ", Z)
 
 #impedance de la ligne de transmission
-Zt = sqrt(50 * Z)
-print ("Impedance de la ligne: ", Zt)
-
-Y0 = (L/pi) * acos(sqrt(50/Zt))
-print ("Point d'insertion Y0 : ", Y0)
+Z1 = sqrt(50 * Z)
+print ("Impedance de la ligne: ", Z1)
 
 Yf = W / 2
 print ("position du via en m sur axe W (Yf) W/2 : ", Yf)
