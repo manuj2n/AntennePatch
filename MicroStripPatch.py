73bf979c1584a51ad497a65da3c6057d5b8b57ea
manuj2n <manu.j2n@gmail.com>
2014-12-05 22:56:05 +0100

    fix bug

--- MicroStripPatch.py
+++ MicroStripPatch.py
@@ -4,11 +4,10 @@ c = 3.0 * pow(10,8)
 #Er = 4.25
 print("entrer la constante Dielectrique (4.25 pour FR4) ")
 Er = float(input())
-#Fr = 440000000.0
 print("entrer la frequence en MHz")
 Fr = float(input()) * 1000000.0
 print("entrer l'epaisseur du dielectrique en mm")
-h = float(input()) / 1000.0 # h = 0.0053 epaisseur du dielectric FR4
+h = float(input()) / 1000.0 # h=0.0035 epaisseur du cuivre sur dielectric FR4
 K0 = 2 * pi * (Fr/c)
 
 def Lamda():
@@ -29,20 +28,20 @@ DeltaL = 0.412 * h * ((Ereff + 0.3) / (Ereff - 0.258)) * (((W / h) + 0.264) / ((
 print ("Delta L : ", DeltaL)
 
 Leff = c/(2 * Fr * sqrt(Ereff))
-print ("L efficace Leff", Leff)
+print ("L effectif Leff", Leff)
 
 L = Leff - (2 * DeltaL)
-print ("dimension L (m) : ", L)
-print ("dimension W (m): ", W)
+print ("dimension L (mm) : ", L * 1000)
+print ("dimension W (mm): ", W * 1000)
 
 Lg = 6.0 * h + L
-print ("Lg : ", Lg)
+print ("Lg (plan de masse)en mm : ", Lg * 1000)
 
 Wg = 6.0 * h + W
-print ("Wg : ", Wg)
+print ("Wg (plan de masse) en mm : ", Wg * 1000)
 
 Xf2 = (L/pi * acos(sqrt(3)/3))
-print ("position du via en m sur axe L (Xf) a partir du bord : ", Xf2)
+print ("position de l'insersion en mm sur axe L (Xf) a partir du bord : ", Xf2 * 1000)
 
 Z = 300.0 * (cos((pi * Xf2)/L) * cos((pi * Xf2)/L))
 print ("Impedance du patch: ", Z)
@@ -51,8 +50,5 @@ print ("Impedance du patch: ", Z)
 Z1 = sqrt(50 * Z)
 print ("Impedance de la ligne: ", Z1)
 
-# Impedance Z0 = 300 * cos^2 ((pi * x) / L)
-# pour Z0 = 50 ohms
-
 Yf = W / 2
-print ("position du via en m sur axe W (Yf) W/2 : ", Yf)
+print ("position de l'insersion en mm sur axe W (Yf) W/2 : ", Yf * 1000)
